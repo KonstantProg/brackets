cf0211de190ae552ce70d5b60ab22c30656eaee9
Konstantin Ahremchuk <ahremchuk.kostya@gmail.com>
2024-09-20 12:45:05 +0300

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const newBrackets = bracketsConfig.map(el => el.join(''));
+  return changeBrackets(str, newBrackets);
+
+}
+
+const changeBrackets = (strBase, newBrackets) => {
+  let strCopy = strBase;
+
+  for (let item of newBrackets) {
+    strCopy = strCopy.replace(item, '');
+  }
+
+  if (strCopy.length === 0) {
+    return true;
+  } else if (strBase === strCopy) {
+    return false;
+  } else {
+    return changeBrackets(strCopy, newBrackets);
+  }
 }
